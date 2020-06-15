cc469b09ae8150bc0e190ed492f234657378e697
mailmaul <62276420+mailmaul@users.noreply.github.com>
2020-06-15 20:13:14 +0700

    UP

---
+++ Minggu_12/application/views/welcome_message.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">Tugas Minggu ke 12</h1>
+		<p class="lead">Enrico Irawan A12.2018.06085</p>
+	</div>
+</div>
